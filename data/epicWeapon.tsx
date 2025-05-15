133375c449eb126754f4f5c1e30732b769899f81
rickmff <rickmff@gmail.com>
2025-05-15 18:47:54 +0100

    Fix image paths in epicWeapon data structure for consistency with capitalization

--- data/epicWeapon.tsx
+++ data/epicWeapon.tsx
@@ -4,7 +4,7 @@ import { StaticImageData } from "next/image"
 const weaponImagePaths = {
   Spear_Epic: "/images/weapons/Stunlock_Icon_SteelSpear02_Epic.png",
   Axe_Epic: "/images/weapons/Stunlock_Icon_SteelAxe02_Epic.png",
-  Greatsword_Epic: "/images/weapons/Stunlock_Icon_SteelGreatsword02_Epic.png",
+  Greatsword_Epic: "/images/weapons/Stunlock_Icon_SteelGreatSword02_Epic.png",
   Crossbow_Epic: "/images/weapons/Stunlock_Icon_SteelCrossbow02_Epic.png",
   Pistols_Epic: "/images/weapons/Stunlock_Icon_Steel_Pistol02_Epic.png",
   Reaper_Epic: "/images/weapons/Stunlock_Icon_SteelScythe02_Epic.png",
@@ -12,7 +12,7 @@ const weaponImagePaths = {
   Mace_Epic: "/images/weapons/Stunlock_Icon_SteelMace02_Legendary_Epic.png",
   Whip_Epic: "/images/weapons/Stunlock_Icon_SteelWhip02_Epic.png",
   Longbow_Epic: "/images/weapons/Stunlock_Icon_SteelBow02_Epic.png",
-  Twinblades_Epic: "/images/weapons/Stunlock_Icon_SteelTwinblades02_Epic.png",
+  Twinblades_Epic: "/images/weapons/Stunlock_Icon_SteelTwinBlades02_Epic.png",
   Claws_Epic: "/images/weapons/Stunlock_Icon_SteelClaws02_Epic.png",
   Daggers_Epic: "/images/weapons/Stunlock_Icon_SteelDaggers02_Epic.png",
   Slashers_Epic: "/images/weapons/Stunlock_Icon_SteelSlashers02_Epic.png",
