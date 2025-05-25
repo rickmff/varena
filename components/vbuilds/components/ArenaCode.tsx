4493903f58716568214efe91beb16c4d0a71e015
Kris Coulson <kriscoulson@gmail.com>
2025-05-25 01:27:48 -0700

    and readOnly prop to input

--- components/vbuilds/components/ArenaCode.tsx
+++ components/vbuilds/components/ArenaCode.tsx
@@ -29,6 +29,7 @@ const ArenaCode: React.FC = () => {
   return (
     <div className="flex gap-4">
       <input
+        readOnly
         className="text-gray-300 text-base bg-black/50 bg-black px-4 py-2 rounded-md border w-3/4 text-center"
         value={exportCommand}
       />
