45575d5da0bc04609c0ef9296bf5d5cfaac5d7fc
rickmff <rickmff@gmail.com>
2026-03-04 20:10:56 +0000

    fix headers

--- next.config.mjs
+++ next.config.mjs
@@ -55,6 +55,23 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+      {
         source: '/api/image',
         headers: [
           {
