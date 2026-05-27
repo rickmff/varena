9e9376364d75c7424c01cbaf93c1c9dacffa83d7
RendyMod <rendytgnp@laposte.net>
2026-05-27 19:14:19 +0200

    Updated Raidable Plots

--- data/raidable-plots.ts
+++ data/raidable-plots.ts
@@ -1,6 +1,6 @@
 // Plot IDs that have a castle and can be raided.
 // Add new IDs to this list as castles appear.
 export const RAIDABLE_PLOTS: ReadonlySet<number> = new Set([
-	38, 40, 45, 70, 89, 90, 95, 98, 100, 107, 108, 116, 123, 124, 125, 136, 143,
-	145,
+	11, 13, 22, 23, 26, 38, 40, 43, 45, 51, 57, 62, 64, 70, 89, 90, 95, 98, 100,
+	101, 106, 107, 108, 111, 116, 117, 123, 124, 125, 136, 137, 143, 145,
 ]);
